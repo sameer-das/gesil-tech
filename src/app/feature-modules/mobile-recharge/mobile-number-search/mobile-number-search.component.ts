e25c237cc39b5977839c4867a0e147c766010e7b
sameer-das <playsameer979@gmail.com>
2024-08-12 09:31:24 +0530

    Fixed issue with recent trans in mobile prepaid recharge

--- src/app/feature-modules/mobile-recharge/mobile-number-search/mobile-number-search.component.ts
+++ src/app/feature-modules/mobile-recharge/mobile-number-search/mobile-number-search.component.ts
@@ -6,6 +6,7 @@ import { PopupService } from 'src/app/popups/popup.service';
 import { environment } from 'src/environments/environment';
 import { MobileRechargeService } from '../mobile-recharge.service';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import * as convert from 'xml-js';
 
 
 @Component({
@@ -36,7 +37,7 @@ export class MobileNumberSearchComponent implements OnInit, OnDestroy {
     commission: boolean = true;
     commissionTooltipMessage = "Disabling commission will not credit any commission to wallet."
 
-    ngOnInit (): void {
+    ngOnInit(): void {
         this.getRecentTrans()
 
         this.mobileNo.valueChanges.subscribe({
@@ -49,19 +50,19 @@ export class MobileNumberSearchComponent implements OnInit, OnDestroy {
         })
     }
 
-    ngOnDestroy (): void {
+    ngOnDestroy(): void {
         this.$destory.next(true)
     }
 
     mobile_operator_to_logo: any[] = [
-        { operator: 'airtel', logo: 'airtel-logo.jpg' },
-        { operator: 'jio', logo: 'jio.png' },
-        { operator: 'bsnl', logo: 'bsnl-logo.svg' },
-        { operator: 'voda', logo: 'vi-logo.svg' },
-        { operator: 'vi', logo: 'vi-logo.svg' },
+        { operator: 'airtel', logo: 'airtel-logo.jpg', BBPSBillerID: 'BILAVAIRTEL001', BBPSBillerName: 'Airtel' },
+        { operator: 'jio', logo: 'jio.png', BBPSBillerID: 'BILAVJIO000001', BBPSBillerName: 'Jio' },
+        { operator: 'bsnl', logo: 'bsnl-logo.svg', BBPSBillerID: 'BILAVBSNL00001', BBPSBillerName: 'BSNL' },
+        { operator: 'voda', logo: 'vi-logo.svg', BBPSBillerID: 'BILAVVI0000001', BBPSBillerName: 'VI' },
+        { operator: 'vi', logo: 'vi-logo.svg', BBPSBillerID: 'BILAVVI0000001', BBPSBillerName: 'VI' },
     ]
 
-    onInput () {
+    onInput() {
         this.isInputInvalid = false;
         this.inputInvalidMessage = `Don't add +91 or 0 before your mobile number!`;
         this.showCta = false;
@@ -117,7 +118,7 @@ export class MobileNumberSearchComponent implements OnInit, OnDestroy {
 
     }
 
-    onFormSubmit () {
+    onFormSubmit() {
         if (this.mobileNo.invalid)
             return;
         this._router.navigate(['mobile-recharge/plans']);
@@ -125,7 +126,7 @@ export class MobileNumberSearchComponent implements OnInit, OnDestroy {
 
     previousTransactions: any[] = [];
 
-    getRecentTrans () {
+    getRecentTrans() {
 
         this._mobileService.getRecentPrepaidRechargeTransactions(this.currentUser.user.user_EmailID, 1, 1)
             .pipe(
@@ -134,29 +135,47 @@ export class MobileNumberSearchComponent implements OnInit, OnDestroy {
             )
             .subscribe({
                 next: (resp: any) => {
-                    // console.log(resp)
+                    console.log(resp)
                     if (resp.code === 200 && resp.status === "Success" && resp?.data?.length > 0) {
                         this.previousTransactions = resp?.data?.map((curr: any) => {
-                            const req = curr.wallet_transaction_request && JSON.parse(curr.wallet_transaction_request);
-                            // console.log(req)
-                            let operatorDetails: any;
-                            let foundOp: any;
-                            if (req) {
-                                operatorDetails = environment.PREPAID_RECHARGE_OPERATOR.find(curr => +curr.op === +req.op)
-                                // console.log(operatorDetails);
-                                foundOp = this.mobile_operator_to_logo.filter((op: any) => op.operator.includes(operatorDetails?.provider?.trim().toLowerCase()))
-                                // this.currentOperatorLogo = foundOp?.length > 0 ? `./assets/logo/${foundOp[0].logo}` : '';
+                            console.log(curr)
+                            
 
+                            if (curr.wallet_transaction_recall === 'BBPS') {
+                                // IF BBPS 
+                                const req = this.getTransDetailsAsJSON(curr);
+                                const found = this.mobile_operator_to_logo.find(curr => curr.BBPSBillerID === req.billerId)
+                                return {
+                                    ...req,
+                                    operatorLogo: `./assets/logo/${found?.logo}`,
+                                    operatorName: found.BBPSBillerName
+                                }
+
+                            } else {
+                                // IF Munimulti
+                                const req = curr.wallet_transaction_request && JSON.parse(curr.wallet_transaction_request);
+                                let operatorDetails: any;
+                                let foundOp: any;
+                                if (req) {
+                                    operatorDetails = environment.PREPAID_RECHARGE_OPERATOR.find(curr => +curr.op === +req.op)
+                                    // console.log(operatorDetails);
+                                    foundOp = this.mobile_operator_to_logo.filter((op: any) => op.operator.includes(operatorDetails?.provider?.trim().toLowerCase()))
+                                    // this.currentOperatorLogo = foundOp?.length > 0 ? `./assets/logo/${foundOp[0].logo}` : '';
+
+                                }
+
+                                return {
+                                    ...JSON.parse(curr.wallet_transaction_Logfile),
+                                    operatorLogo: foundOp?.length > 0 ? `./assets/logo/${foundOp[0].logo}` : '',
+                                    operatorName: operatorDetails ? operatorDetails?.provider : ''
+                                }
                             }
-                            return {
-                                ...JSON.parse(curr.wallet_transaction_Logfile),
-                                operatorLogo: foundOp?.length > 0 ? `./assets/logo/${foundOp[0].logo}` : '',
-                                operatorName: operatorDetails ? operatorDetails?.provider : ''
-                            }
+
+
+
                         })
                     }
 
-                    // console.log(this.previousTransactions)
                 },
                 error: (err) => {
                     console.log(err)
@@ -164,24 +183,35 @@ export class MobileNumberSearchComponent implements OnInit, OnDestroy {
             })
     }
 
-    onRecentTransactionCardClick (trans: any) {
+    onRecentTransactionCardClick(trans: any) {
         // console.log(trans)
         this.mobileNo.setValue(trans.mn);
         // console.log(this.mobileForm)
 
     }
 
-    goToDashboard () {
+    goToDashboard() {
         this._router.navigate(['dashboard'])
     }
 
 
-    onToggle (e: MatSlideToggleChange) {
+    onToggle(e: MatSlideToggleChange) {
         this.commission = e.checked;
-        if(this.commission) {
+        if (this.commission) {
             this.commissionTooltipMessage = "Disabling commission will not credit any commission to the wallet."
         } else {
             this.commissionTooltipMessage = "Enabling commission will credit appropriate commission to the wallet."
+        }
+    }
+
+
+    getTransDetailsAsJSON(trans: any) {
+        const j = convert.xml2json(trans['wallet_transaction_request'], { compact: true });
+        const json = JSON.parse(j);
+        return {
+            billerId: json.billPaymentRequest.billerId['_text'],
+            amt: json.billPaymentRequest.amountInfo.amount['_text'] / 100, // to convert to rupees
+            mn: json.billPaymentRequest.inputParams.input.paramValue['_text']
         }
     }
 
